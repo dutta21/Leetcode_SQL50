21c7ba8dab6032bf7ec9b3644d8655118d50a630
Aritra <duttaaritra30@gmail.com>
2026-07-04 14:39:18 +0530

    Q19

--- 1174.sql
+++ 1174.sql
@@ -1,6 +1,7 @@
-select 100.0*round((avg(case when order_date=customer_pref_delivery_date then 1 else 0 end)),2)
+select round((avg(case when order_date=customer_pref_delivery_date then 1 else 0 end))*100.0,2)
 as immediate_percentage
 from 
 (select min(order_date) as order_date, min(customer_pref_delivery_date) as customer_pref_delivery_date 
 from delivery 
 group by customer_id);
+
